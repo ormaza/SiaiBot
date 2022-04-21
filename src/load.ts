fa32f610d83956a04f1cba5c9e7a1c81650469d7
ormazabal <ormazabalnascimento@gmail.com>
2022-04-21 15:56:46 -0300

    salvando..

--- src/load.ts
+++ src/load.ts
@@ -10,14 +10,10 @@ export function loadLanguage(manager: any){
   manager.addAnswer('pt', 'saudacao', '{{saudacoes}}. Como posso ajudá-lo?');
   manager.addAnswer('pt', 'saudacao', '{{saudacoes}}. Em que posso ser útil?');
 
-  manager.addDocument('pt', 'ajuda', 'sistema.obter');
-  manager.addDocument('pt', 'siai', 'sistema.obter');
+  manager.addDocument('pt', '%preciso% obter ajuda', 'sistema.obter');
   manager.addAnswer('pt', 'sistema.obter', 'Entendido. Para qual sistema SIAI você necessita de ajuda?');
 
-  manager.addDocument('pt', 'não era isso', 'sistema.negativo');
-  manager.addAnswer('pt', 'sistema.negativo', 'Desculpe o mau entendido. Para qual sistema SIAI você necessita de ajuda?');
-
-  manager.addDocument('pt', 'ajuda sobre %siaiconcursos%', 'concursos.obter');
+  manager.addDocument('pt', 'siai concursos', 'concursos.obter');
   manager.addAnswer('pt', 'concursos.obter', 'Entendido. Qual a sua dúvida sobre o SIAI Concursos?');
 
   manager.addDocument('pt', 'cadastro concurso', 'concursos.cadastro.concurso');
@@ -26,25 +22,36 @@ export function loadLanguage(manager: any){
                     '2. No menu lateral, selecione CONCURSOS e em seguida, CADASTRAR NOVO CONCURSO');
 
   manager.addDocument('pt', 'cadastro ato concurso', 'concursos.cadastro.ato');
-  manager.addAnswer('pt', 'concursos.cadastro.ato', 'Existe algumas formas de cadastrar um concurso: \n' +
+  manager.addAnswer('pt', 'concursos.cadastro.ato', 'Existe algumas formas de cadastrar um  ato concurso: \n' +
                     '1. Na tela inicial, clique no botão CONSULTAR LISTAGEM DE CONCURSOS CADASTRADOS \n' +
                     '1.1 Escolha um dos concursos da lista, e clica no botão Ato do Concurso \n' +
                     '2. No menu lateral, selecione CONCURSOS e em seguida, CONSULTAR LISTAGEM DE CONCURSOS CADASTRADOS e siga o passo 1.1 já descrito');
+
+
+
+
+
+
+
+
+  manager.addDocument('pt', 'não era isso', 'sistema.negativo');
+  manager.addAnswer('pt', 'sistema.negativo', 'Desculpe o mau entendido :-(');
+
   return manager;
 }
 
 function loadEntities(manager: any){
   manager.addNamedEntityText(
-    'siaiconcursos',
-    'siai concursos',
-    ['pt'],
-    ['Siai Concursos', 'siaiconcursos', 'siai-concursos', 'siai concurso', 'concursos'],
-  );
-  manager.addNamedEntityText(
     'saudacoes',
     'oi',
     ['pt'],
     ['oi', 'bom dia', 'boa tarde', 'boa noite', 'e ae', 'olá'],
+  );
+  manager.addNamedEntityText(
+    'preciso',
+    'quero',
+    ['pt'],
+    ['quero', 'preciso', 'necessito'],
   );
 
   return manager;
@@ -68,7 +75,7 @@ export function log(response: any, message: Message, client: Whatsapp, callAPI: 
     telefone = message.from.substring(2,4) + '9' + message.from.substring(4,message.from.length-5);
   }
   if(response.intent === "None"){
-      msgEnviada = "[Whatsapp Bot] Desculpa. Não consegui te entender."
+      msgEnviada = "Desculpa. Não consegui te entender."
   }
   
   if(callAPI){
