ad84c3c56fca01539ec21999605624124340af30
ormazabal <ormazabalnascimento@gmail.com>
2023-03-11 01:36:58 -0300

    question generator

---
+++ qg/README.md
@@ -0,0 +1,8 @@
+# Question Generator
+
+baseado nos estudos de https://github.com/AMontgomerie/question_generator
+
+##### alguns comandos
+pip install -r requirements.txt -qq
+python run_qg.py --in_file data/caj.txt --num_questions 2 --answer_style sentences
+python run_qg.py --in_file data/caj.csv --out_csv data/out.csv --num_questions 2 --answer_style sentences 
---
+++ qg/run_qg.py
@@ -0,0 +1,74 @@
+import argparse
+import csv
+from questiongenerator import QuestionGenerator
+from questiongenerator import print_qa
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--answer_style",
+        default="all",
+        type=str,
+        help="The desired type of answers. Choose from ['all', 'sentences', 'multiple_choice']",
+    )
+    parser.add_argument("--model_dir", type=str, default=None)
+    parser.add_argument("--num_questions", type=int, default=10)
+    parser.add_argument("--show_answers", dest="show_answers", action="store_true", default=True)
+    parser.add_argument("--in_file", type=str, required=True)
+    parser.add_argument("--use_qa_eval", dest="use_qa_eval", action="store_true", default=True)
+    parser.add_argument("--x", type=int, default=0)
+    parser.add_argument("--y", type=int, default=0)
+    parser.add_argument("--out_csv", type=str, required=False)
+    return parser.parse_args()
+
+def read_cell(x, y):
+    args = parse_args()
+    with open(args.in_file, 'r') as f:
+        reader = csv.reader(f)
+        y_count = 0
+        for n in reader:
+            if y_count == y:
+                cell = n[x]
+                return cell
+            y_count += 1
+
+def write_cell(data):
+    args = parse_args()
+    file = open(args.out_csv, 'w+', newline ='')
+    with file:   
+        write = csv.writer(file)
+        write.writerows(data)
+
+if __name__ == "__main__":
+    args = parse_args()
+    qg = QuestionGenerator()
+    filename = args.in_file
+    if filename[-3:] == "txt":
+        with open(args.in_file, 'r', encoding="utf8") as file:
+            Lines = file.readlines()
+        for line in Lines:
+            qa_list = qg.generate(
+                line,
+                num_questions=int(args.num_questions),
+                answer_style=args.answer_style,
+                use_evaluator=args.use_qa_eval
+            )
+            print_qa(qa_list, show_answers=args.show_answers)
+    else:
+        i = args.x
+        list_qa = []
+        while read_cell(args.y, i) != None:
+            print(read_cell(args.y, i))
+            qa_list = qg.generate(
+                    read_cell(args.y, i),
+                    num_questions=int(args.num_questions),
+                    answer_style=args.answer_style,
+                    use_evaluator=args.use_qa_eval
+                )
+            # print_qa(qa_list, show_answers=args.show_answers)
+            list_qa.append(qa_list)
+            i+=1
+        write_cell(list_qa)
+
+
